70e18c6fefbf12a0e90cae5d05f770bccd989183
maquattrocchi <martinaquattrocchi5@gmail.com>
2022-05-06 17:09:10 +0200

    added HeaderComponent

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="d-flex align-items-center">
+        <img src="../assets/images/spotify-logo.png" alt="spotify-logo">
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent',
+}
+</script>
+
+<style scoped lang="scss">
+@import '../style/vars';
+@import '../style/general';
+    header{
+        background: $card-color;
+        height: 70px;
+        
+        img{
+            margin-left: 1rem;
+            width: 55px;
+        }
+    }
+</style>
